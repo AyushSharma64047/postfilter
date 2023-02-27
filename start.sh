6f05d0e7e743f04483447a80c80850b2fcd817ce
Sawansingh24 <119250143+Sawansingh24@users.noreply.github.com>
2023-02-28 00:52:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone $ORIGINAL_REPO /Filter-Bot
+  git clone https://github.com/Sawansingh24 .git /Btmanjusms
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Filter-Bot
+  git clone $UPSTREAM_REPO /Btmanjusms
 fi
-cd /Filter-Bot
+cd /Btmanjusms
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
