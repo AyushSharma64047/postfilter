9dc01e7c976b0e2a41d7e8a9b31bb5f081d154e9
Ayush Sharma <106085120+AyushSharma64047@users.noreply.github.com>
2023-04-18 16:48:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sawansingh24 /Btmanjusms
+  git clone https://github.com/AyushSharma64047/postfilter /postfilter 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Btmanjusms
+  git clone $UPSTREAM_REPO /postfilter
 fi
-cd /Btmanjusms
+cd /postfilter 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+gunicorn app:app & python3 bot.py
